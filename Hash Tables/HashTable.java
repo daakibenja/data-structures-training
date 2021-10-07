27ffe1b634df9c50c84d41624d18088869ce7718
= <=>
2021-10-07 11:05:04 +0500

    error reporting

--- Hash Tables/HashTable.java
+++ Hash Tables/HashTable.java
@@ -1,5 +1,6 @@
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,14 +14,18 @@ public class HashTable {
         }
     }
 
+    /**
+     * This method takes a line of words and extracts the 
+     * individaul words into an arraylist and returns the 
+     * arraylist
+     * @param line
+     * @return ArrayList<String>
+     */
     private static ArrayList<String> getWordsFromLine(String line) {
 
         ArrayList<String> words = new ArrayList<String>();
         try (Scanner rowScanner = new Scanner(line)) {
-            // Ofcourse these values are separated by commas
-            // line = line.replaceAll("-?\b+", "");
-            // rowScanner.useDelimiter(" ");
-            // Then get all the values
+            
             while (rowScanner.hasNext()) {
                 String word = rowScanner.next();
                 if (word.matches("[a-zA-Z]+")) {
@@ -32,41 +37,37 @@ public class HashTable {
 
             }
         }
-        // return the values in an array.
+        
         return words;
     }
 
+    /**
+     * Method to extract all words(only valid words) 
+     * int an array of strings(words).
+     * valid words are, words consisting of only alphabets
+     */
     public static String[] getAllWords(String fileName) {
-        // An arraylist to conatain all the songs as instaces of the song object
+    
         ArrayList<String> allWords = new ArrayList<String>();
-        // This will enable us skip the heading in the csv file
-        // File name make sure you use the right path.
+        
 
         try (Scanner scanner = new Scanner(new File(fileName));) {
             while (scanner.hasNextLine()) {
-
-                // Skip heading that's when the skipHeading var
-
-                // Temporary array to hold each song's data
-                // song id will be index 0 and views index 1
+               
                 ArrayList<String> wordsInLine = getWordsFromLine(scanner.nextLine());
 
-                // Create a song object using the song details
-                // Song song = new Song(songDetails.get(0), songDetails.get(1));
-
-                // Push the song to the all songs arraylist
-                // Then you are good to go!!!!!
                 allWords.addAll(wordsInLine);
             }
 
-        } catch (Exception e) {
+        } catch (FileNotFoundException r) {
             // Exception handling perhaps wrong file name
-            System.out.println(e);
+            r.getMessage();
+            // System.out.println(e);
         }
-        // Final songs array with size as all songs array list
+    
         String[] finalWordsArray = new String[allWords.size()];
 
-        // Populate the final songs array and return it
+    
         finalWordsArray = allWords.toArray(finalWordsArray);
         return finalWordsArray;
 
